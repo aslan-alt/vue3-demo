cd7200353adff562a6526d4b11751a932aac2b94
aslan-alt <874470891@qq.com>
2020-10-25 23:03:54 +0800

    添加Home页面样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <top-nav />
+    <top-nav/>
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -14,14 +14,37 @@
 
 <script>
 import TopNav from "../components/TopNav.vue";
+
 export default {
   name: "Home.vue",
-  components:{
+  components: {
     TopNav
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
 
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
